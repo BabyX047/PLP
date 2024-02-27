4dfefe10fbcdd82216aac444b5cfadcf9de04dde
Algorithms <76278910+BabyX047@users.noreply.github.com>
2024-02-27 14:48:57 +0300

    Add files via upload

---
+++ Dart Dev/Variable.dart
@@ -0,0 +1,16 @@
+void main() {
+  //declaring variables
+  String name = "Lewis";
+  String address = "Kenya";
+  num age = 25;
+  num height = 6.1;
+  bool ismarried = false;
+
+  //printing variable values
+  print ("My name is $name");
+  print ("I Live in $address");
+  print ("I am $age years old");
+  print ("I am $height tall");
+  print ("Am I married? $ismarried");
+}
+// testing firt commit/push
