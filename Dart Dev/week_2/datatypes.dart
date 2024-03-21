2b6c9fa726501d4166b38a6935967bd4bd45dd9e
Algorithms <76278910+BabyX047@users.noreply.github.com>
2024-03-21 11:58:57 +0300

    Add files via upload

---
+++ Dart Dev/week_2/datatypes.dart
@@ -0,0 +1,29 @@
+void main() {
+  // Integer data type (int)
+  int age = 30;
+  print('Age: $age');
+  
+  // Double data type (double)
+  double height = 5.9;
+  print('Height: $height');
+  
+  // String data type (String)
+  String name = 'John';
+  print('Name: $name');
+
+  //Boolean data type (boolean)
+  bool isMarried = true;
+  print('Is married: $isMarried');
+  
+  // List data type (List)
+  List<int> numbers = [1, 2, 3, 4, 5];
+  print('Numbers: $numbers');
+  
+  // Map data type (Map)
+  Map<String, dynamic> person = {
+    'name': 'Alice',
+    'age': 25,
+    'height': 5.6
+  };
+  print('Person: $person');
+}
